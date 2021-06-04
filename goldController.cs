e110db93b695007da00bd1cbc6f6cca29ddb0870
feddinara <60204909+feddinara@users.noreply.github.com>
2021-06-04 03:17:06 +0300

    Add files via upload

---
+++ goldController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goldController : MonoBehaviour
+{
+    AudioSource audio;
+    MeshRenderer display;
+    static int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        display = GetComponent<MeshRenderer>();
+        audio.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseDown()
+    {
+        audio.Play();
+        display.enabled = false;
+        score = score + 1;
+        print(score);
+        print("Попал!");
+        if(score > 3){
+           print("Ты выиграл!");
+        }
+    }
+}
---
+++ noGoldController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class noGoldController : MonoBehaviour
+{
+    AudioSource audio;
+    MeshRenderer display;
+    static int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        display = GetComponent<MeshRenderer>();
+        audio.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseDown()
+    {
+        audio.Play();
+        display.enabled = false;
+        print("Мимо");
+        if(score > 11){
+           print("Ты проигарл :(");
+        }
+    }
+}
